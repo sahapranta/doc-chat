02acb92a27c116e252bc5a290fadd59731364b96
sahapranta <pranta1204@gmail.com>
2024-04-08 17:05:55 +0600

    added ci/cd pipeline using github action

---
+++ .scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Pull the latest version of the app from the production branch
+git pull origin production
+
+# Build the Docker image and tag it
+docker build -t my-node-app .
+
+# Stop the container if it's running
+if [ "$(docker ps -q -f name=node_chat)" ]; then
+    echo "Stopping existing node_chat container..."
+    docker stop node_chat
+fi
+
+# Remove the container if it exists
+if [ "$(docker ps -aq -f status=exited -f name=node_chat)" ]; then
+    echo "Removing existing node_chat container..."
+    docker container rm node_chat
+fi
+
+# Run the Docker container
+docker run -d -p 3001:3000 --name node_chat my-node-app
+
+echo "Deployment finished!"
